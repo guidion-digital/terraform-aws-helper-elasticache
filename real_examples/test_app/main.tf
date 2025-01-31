7918466c6423f3a9bcf151a911e626fbfac8c5a5
Afraz Ahmadzadeh <afrazo@proton.me>
2025-01-31 10:53:35 +0100

    chore: Improvements to example

--- real_examples/test_app/main.tf
+++ real_examples/test_app/main.tf
@@ -5,7 +5,9 @@ module "memcached" {
   application_name = "test-app"
   stage            = "dev"
 
-  engine = "memcached"
+  engine         = "memcached"
+  engine_version = "1.6.16"
+  node_type      = "cache.t3.micro"
 
   # Use 'availablity_zone' if az_mode is 'single-az'
   az_mode                      = "cross-az"
@@ -13,6 +15,13 @@ module "memcached" {
 
   vpc_id     = "vpc-0dfab25adab0ea4d5"
   subnet_ids = ["subnet-0c778aad2e6e3b88b", "subnet-0a5208d551253a002", "subnet-0720e2396dc948d72"]
+
+  parameters = [
+    {
+      name  = "maxmemory-policy"
+      value = "noeviction"
+    }
+  ]
 
   # There are a number of ways to control access to the cluster. By default, setting
   # only vpc_id as above means that the entire VPC CIDR will have access.
